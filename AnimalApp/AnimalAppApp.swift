2d9a1abc58f7d5b95f2c8e8cf6dfeeaad7182fde
Mohak Parmar <mohak@topsinfosolutions.com>
2021-08-03 11:12:32 +0530

    Initial Commit

---
+++ AnimalApp/AnimalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalAppApp.swift
+//  AnimalApp
+//
+//  Created by Mohak Parmar on 03/08/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
